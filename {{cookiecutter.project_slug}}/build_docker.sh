a4d64f3063e349782e9a6d4db5fa831965e0a31b
gpascualg <gpascualg93@gmail.com>
2017-02-17 11:22:18 +0100

    Add dist directory and distributable docker images

--- {{cookiecutter.project_slug}}/build_docker.sh
+++ {{cookiecutter.project_slug}}/build_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t {{cookiecutter.project_slug}}/dataservice:latest -f docker/Dockerfile .
+docker build -t {{cookiecutter.project_slug}}:latest -f docker/Dockerfile .
+docker save -o dist/{{cookiecutter.project_slug}}.tar {{cookiecutter.project_slug}}
--- {{cookiecutter.project_slug}}/run_docker.sh
+++ {{cookiecutter.project_slug}}/run_docker.sh
@@ -2,4 +2,4 @@
 
 docker stop {{cookiecutter.project_slug}}
 docker rm {{cookiecutter.project_slug}}
-docker run --name {{cookiecutter.project_slug}} -d -p 8080:80 {{cookiecutter.project_slug}}/dataservice
+docker run --name {{cookiecutter.project_slug}} -d -p 8080:80 {{cookiecutter.project_slug}}
