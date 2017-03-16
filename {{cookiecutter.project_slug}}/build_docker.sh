632f42f4017e77da6a3bc082cc18357d60460633
Guillem Pascual Guinovart <gpascualg93@gmail.com>
2017-03-16 22:27:31 +0100

    Build dist directory if it doesn't exist

--- {{cookiecutter.project_slug}}/build_docker.sh
+++ {{cookiecutter.project_slug}}/build_docker.sh
@@ -2,4 +2,5 @@
 
 rm dist/{{cookiecutter.project_slug}}.tar
 docker build -t {{cookiecutter.project_slug}}:latest -f docker/Dockerfile .
+make -p dist
 docker save -o dist/{{cookiecutter.project_slug}}.tar {{cookiecutter.project_slug}}
